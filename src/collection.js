1d2a49ee8267d188d9ef2ef42bdf8c44e94e0afc
uchu <pavel.burylichev@gmail.com>
2023-03-03 20:45:16 +0530

    Added second indexer

--- src/collection.js
+++ src/collection.js
@@ -122,6 +122,56 @@ export default [
         }
     },
     {
+        type: ['Gauge', 'Gauge', 'Gauge'],
+        name: ['sgt_graph_active_subs', 'sgt_graph_deprecated_subs', 'sgt_graph_alloc_ages'],
+        help: ['SGTstake Active subgraphs count', 'SGTstake Deprecated subgraphs count', 'SGTstake Allocations block age'],
+        url: process.env.NETWORK_SUBGRAPH,
+        query: `fragment IndexerAllocationFragment on Allocation {
+                  id
+                  status
+                  createdAtBlockNumber
+                  subgraphDeployment {
+                    ipfsHash
+                    versions(first: 1000, where: {entityVersion: 2}) {
+                      id
+                      subgraph(where: {status: Active}) {
+                        id
+                        active
+                        displayName
+                        currentVersion { id subgraphDeployment { id } }
+                      }
+                    }
+                  }
+                }
+                {
+                  allocations(
+                    first: 1000
+                    skip: 0
+                    orderBy: createdAt
+                    orderDirection: desc
+                    where: {indexer: "${process.env.INDEXER_ADDRESS2}", status: Active}
+                  ) { ...IndexerAllocationFragment }
+                }`,
+        callback: (response, prometheus) => {
+            prometheus[0].set(Number(response.data.data.allocations.length));
+
+            prometheus[1].set(response.data.data.allocations.filter(
+                allocation => {
+                    return allocation.subgraphDeployment.versions[0].id !==
+                        allocation.subgraphDeployment.versions[0].subgraph.currentVersion.id;
+                }).length);
+
+            if (lastBlock) {
+                for (const allocation of response.data.data.allocations) {
+                    const subgraphName = allocation.subgraphDeployment.versions[0].subgraph.displayName;
+                    const allocationId = allocation.subgraphDeployment.ipfsHash;
+                    const ageInBlocks = lastBlock - Number(allocation.createdAtBlockNumber);
+                    prometheus[2].labels(allocationId, subgraphName).set(ageInBlocks);
+                }
+            }
+        }
+    },
+    {
         type: 'Counter',
         name: 'indexer_url_downtime_counter',
         help: 'Downtime counter of indexer URL',
